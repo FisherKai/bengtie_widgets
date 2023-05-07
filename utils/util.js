9f4da604af63e78115c3bfd8eea905e704beb67d
fisherkaiyu <fisherkaiyu@tencent.com>
2023-05-07 09:15:54 +0800

    feat: 添加request

--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,38 @@ const formatNumber = n => {
   return n[1] ? n : `0${n}`
 }
 
+const formatUrlStr = url =>{
+    const list = url.split("?");
+    const paramsList = list[1] ? list[1].split("&") : [];
+    const result = {}
+    for (let index = 0; index < paramsList.length; index++) {
+        const [key,value] = paramsList[index].split("=");
+        result[key]=value;
+    }
+    return result;
+}
+
+const getUrlValue = (url,key)=>{
+    const valueObj = formatUrlStr(url);
+    return valueObj[key];
+}
+
+const setUrlValue = (url,key,value)=>{
+    const valueObj = formatUrlStr(url);
+    valueObj[key]=value;
+    let str = ""
+    debugger;
+    for (const key in valueObj) {
+        if (Object.hasOwnProperty.call(valueObj, key)) {
+            const element = valueObj[key];
+            str+=`${key}=${element}&`
+        }
+    }
+    return url.split("?")[0]+str.slice(0,str.length-1);
+}
+
 module.exports = {
-  formatTime
+  formatTime,
+  setUrlValue,
+  getUrlValue
 }
